d2461bf8e837ac030213ee5fed971d7b24ae6c3b
Martin Novák <martin.novak@argo22.com>
2026-03-27 12:35:05 +0100

    chore: migrate expo config to app.config.js

---
+++ app.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    expo: {
+        name: "FitApp",
+        slug: "fitapp",
+        version: "0.3.0",
+        orientation: "portrait",
+        icon: "./assets/images/icon.png",
+        scheme: "fitapp",
+        userInterfaceStyle: "automatic",
+        newArchEnabled: true,
+        splash: {
+            image: "./assets/images/splash.png",
+            resizeMode: "contain",
+            backgroundColor: "#607d8b"
+        },
+        ios: {
+            supportsTablet: true,
+            bundleIdentifier: "com.martinnovak22.fitapp"
+        },
+        android: {
+            edgeToEdgeEnabled: true,
+            package: "com.martinnovak22.fitapp"
+        },
+        web: {
+            bundler: "metro",
+            output: "static",
+            favicon: "./assets/images/favicon.png"
+        },
+        plugins: ["expo-router", "expo-sqlite"],
+        experiments: {
+            typedRoutes: true
+        },
+        extra: {
+            dataMode: "local",
+            router: {},
+            eas: {
+                projectId: "66cc05d1-9eb9-457a-b100-f0fa864d965f"
+            }
+        }
+    }
+}
