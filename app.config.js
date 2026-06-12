9bea20af0ce5253b9be6a83f62c2d17904d86577
Martin Novák <martin.novak@argo22.com>
2026-06-12 13:09:17 +0200

    chore: release 0.5.0

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ module.exports = {
     expo: {
         name: 'FitApp',
         slug: 'fitapp',
-        version: '0.4.3',
+        version: '0.5.0',
         orientation: 'portrait',
         icon: './assets/images/icon.png',
         scheme: 'fitapp',
@@ -22,7 +22,7 @@ module.exports = {
         android: {
             edgeToEdgeEnabled: true,
             package: 'com.martinnovak22.fitapp',
-            versionCode: 43,
+            versionCode: 50,
         },
         web: {
             bundler: 'metro',
